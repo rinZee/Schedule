e6e121f5b98786f7e06d3638ee5bf64a91f3d8f3
rinZee <trex33160@gmail.com>
2023-05-22 00:02:42 +0545

    added some content to the main homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className='text-3xl text-sky-500'>Hello World</p>
-    </div>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Make and Share
+        <br />
+        <span className="orange_gradient text-center">
+          A schedule to keep your goals on track
+        </span>
+      </h1>
+
+      <p className="desc text-center">
+        Schedule is an world of people like you who have difficulty staying on
+        track of your goals
+      </p>
+    </section>
+  );
 }
